bc3fc93266188140dc9e2c87c6db7e606cdb6dcf
Tim Shur <tim.shur@gmail.com>
2018-06-29 02:32:49 -0700

    init: initial commit (create-react-app) and basic html

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="splash">
+        <h1>Hello, my name is Tim!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
