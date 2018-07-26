096c261f2107263245407c4e37b692182974d5f1
Tim Shur <tim.shur@gmail.com>
2018-07-26 02:13:57 -0700

    footer: layout contact information

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import styles from './index.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () =>
   <div className={`container ${styles.footer}`}>
-    <h1> Footer </h1>
+    <div className={styles.contact}>
+      <div className={styles.item}>
+        <FontAwesomeIcon icon={faEnvelope} className={styles.icon} size='lg'/>
+        <div className={styles.text}>
+          tim.shur@gmail.com
+        </div>
+      </div>
+      <div className={`${styles.item} ${styles.linkedin}`}>
+        <FontAwesomeIcon icon={faLinkedin} className={styles.icon} size='lg'/>
+        <div className={styles.text}>
+          linkedin.com/in/timshur
+        </div>
+      </div>
+    </div>
   </div>
 
 export default Footer;
