1665b98c3eed699179171ef8a2baa6fb6bc0ac15
Tim Shur <tim.shur@gmail.com>
2019-03-24 21:51:45 -0400

    About: add simple bio

--- src/components/MainPages/About/index.js
+++ src/components/MainPages/About/index.js
@@ -4,7 +4,18 @@ import './index.scss';
 const About = () =>
   <div className='page-container about'>
     <div className='container'>
-      <h1> About </h1>
+      <div className='about-card card-shadow'>
+        <h1 className='title'> About Me </h1>
+        <p className='text'> Hi there! I am a passionate and highly-motivated
+        software engineer with experience in software engineering, machine
+        learning, and full-stack web development. I am currently a student at
+        Santa Clara University studying Computer Science &amp; Engineering and
+        Mathematics. I am grateful to have gotten the opportunity of working with
+        incredible teams at Nike and Headspin doing research and engineering work.
+        Lately, I have been excited to get more into React and develop my expertise
+        with artificial intelligence in Python. Check out some of my recent projects
+        to see what I've been up to! </p>
+      </div>
     </div>
   </div>
 
