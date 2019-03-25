9b677551c355279d4dcb9c89a1be0cfadcbfe0b2
Tim Shur <tim.shur@gmail.com>
2019-03-24 20:13:39 -0400

    Home: update styling and content of home page

--- src/components/MainPages/Home/index.js
+++ src/components/MainPages/Home/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './index.scss';
 
 const Home = () =>
-  <div className='page-container home'>
-    <div className='container'>
-      <h1> Home </h1>
+  <div className='home'>
+    <div className='page-container darken'>
+      <div className='container'>
+        <div className='splash-card'>
+          <h1 className='headline'> Hi, I'm </h1>
+          <h1 className='headline'> Tim Shur. </h1>
+          <h2 className='subtitle'> Machine Learning Engineer, Software Engineer, &amp; Full-Stack Web Developer </h2>
+        </div>
+      </div>
     </div>
   </div>
 
