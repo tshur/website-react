2c2be3a0041dd4617db60d1931611969721b52eb
Tim Shur <tim.shur@gmail.com>
2019-03-24 22:39:34 -0400

    Projects: display project placeholders in a card-grid style

--- src/components/MainPages/Projects/index.js
+++ src/components/MainPages/Projects/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './index.scss';
 
-import Game from '../../../playground/tic-tac-toe/components/Game';
+import projectData from './project-data';
+// import Game from '../../../playground/tic-tac-toe/components/Game';
 
 const Projects = () =>
-  <div className='page-container projects'>
+  <div className='projects'>
     <div className='container'>
-      <h1> Projects </h1>
-      <br />
-      <Game />
+      <div className='projects-body'>
+        <h1 className='title'>Projects</h1>
+        <div className='project-grid'>
+          {projectData.map((project, i) =>
+            <div className='project-card' key={i}>
+              <h1 className='project-name'>{project.name}</h1>
+              <p className='project-desc'>{project.desc}</p>
+              {project.link === '' ? <p className='coming-soon'>Coming soon!</p> : ''}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   </div>
 
---
+++ src/components/MainPages/Projects/project-data.js
@@ -0,0 +1,24 @@
+const projectData = [
+    {
+        'name': 'Tic-Tac-Toe',
+        'link': '',
+        'desc': 'A simple tic-tac-toe game built in React following the official React tutorial.'
+    },
+    {
+        'name': 'Calculator',
+        'link': '',
+        'desc': 'An calculator app built in React based on the iOS phone calculator.'
+    },
+    {
+        'name': 'Reddit Feed',
+        'link': '',
+        'desc': 'An Reddit feed clone built in React. Uses the Reddit API to grab posts from a given subreddit and display them as a list of cards.'
+    },
+    {
+        'name': 'Questioner',
+        'link': '',
+        'desc': 'Web app to help the user brainstorm by asking questions and recording responses. Senior design project (in progress) with Robbie Culkin.'
+    },
+];
+
+export default projectData;
